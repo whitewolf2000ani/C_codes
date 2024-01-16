c3ae912b5aad14f21b6b721a6e04a0f38896d7c9
aniketkumar <kaniket123456789@gmail.com>
2024-01-16 11:45:11 +0530

    used pointer for addition of array elements

---
+++ pointer.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int n,i;
+    int sum=0,*p;
+    printf("enter the number of elements\n");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the elements\n");
+    for(p=&a[0];p<=&a[n-1];p++){
+        scanf("%d",p);
+    }
+
+    for(p=&a[0];p<=&a[n-1];p++){
+        sum+=*p;
+    }
+
+    printf("sum is %d\n",sum);
+
+    return 0;
+}
