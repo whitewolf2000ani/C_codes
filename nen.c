d2fdeb5530d65c745ce8059714e8a82efa978ffc
aniketkumar <kaniket123456789@gmail.com>
2024-03-17 09:54:34 +0530

    added a global variable

--- nen.c
+++ nen.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
+void output();
+int global_var=10;
 int main (){
     int n;
+    global_var=10;
     printf("enter a number\n");
     scanf("%d",&n);
-    printf("Hello the number is:%d\n",n);
+    printf("Hello the number is:%d and global variable is:%d\n",n,global_var);
+    output();
     return 0;
+}
+void output(){
+  printf("global_variable:%d\n",global_var);
 }
 
